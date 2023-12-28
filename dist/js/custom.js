6c54b93e1e736a5a94f720828a0414ad34e1f43f
mukeshkumar1988 <mukesh@cyberpointmedia.com>
2023-12-28 14:55:12 +0530

    pages, draft, published and trash pages done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,10 +1,26 @@
+// dataTable
 $(document).ready(function() {
     $('#example').DataTable();
 
 });
 
+// select all input in datatable
+$(document).ready(function() {
+    $(".selectAll").click(function() {
+        $("table tr input:checkbox").prop('checked', $(this).prop('checked'));
+    });
 
-$('.selectAll').click(function(e) {
-    var table = $(e.target).closest('table');
-    $('tr input:checkbox', table).prop('checked', this.checked);
+    $("table tr input:checkbox").change(function() {
+        if (!$(this).prop("checked")) {
+            $(".selectAll").prop("checked", false);
+        }
+    });
+});
+// show hide password
+$(document).ready(function() {
+    $('.toggle-password').click(function() {
+        $(this).toggleClass("show-password");
+        var input = $("#password");
+        input.attr('type') === 'password' ? input.attr('type', 'text') : input.attr('type', 'password')
+    });
 });
