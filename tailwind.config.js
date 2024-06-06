f1ed8b0e637a0a68c56d1b5b85b8767a36839e9b
mukeshkumar1988 <mukesh@cyberpointmedia.com>
2024-06-06 15:17:03 +0530

    widgets page changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     content: ['./views/**/*.ejs'],
   },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-gradient': 'linear-gradient(180deg, #FFF 0%, #E9F3FF 100%)',
+    },
+    boxShadow: {
+        'custom': '0px 2px 4px -2px rgba(24, 39, 75, 0.12), 0px 4px 4px -2px rgba(24, 39, 75, 0.08)', // Corrected custom box shadow
+    },
+    },
   },
   plugins: [],
 }
